531d89fcfbb404203b24da60bcdb88507eca6e9d
Churro03 <35495503+Churro03@users.noreply.github.com>
2018-01-17 08:29:50 -0700

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <Windows.h>
+
+using namespace std;
+
+int main() {
+
+	cout << "What did the ocean say to the shore?" << endl;
+
+	Sleep(3000);
+
+	cout << "nothing, he just waved." << endl;
+
+	system("pause");
+}
